440614ce52d92f375b4a1c8867886d0a75834377
argentoff1 <mparinos56@gmail.com>
2022-10-10 10:38:36 +0300

    Пейзаж готов

--- app/src/main/java/com/example/landscape/MainActivity.java
+++ app/src/main/java/com/example/landscape/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView sunImageView = (ImageView) findViewById(R.id.sun);
+        // Анимация для восхода солнца
+        Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sunrise);
+        // Подключаем анимацию к нужному изображению View
+        sunImageView.startAnimation(sunRiseAnimation);
     }
 }
